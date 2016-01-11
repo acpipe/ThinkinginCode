f939c2c4cc3de3e6f05f9f9f25b70809b9f09655
flysalmon <1023598954@qq.com>
2016-01-11 20:48:06 +0800

    The exception specification P322

---
+++ hitcs-thinkinginjava/src/main/java/com/cs/hit/thinkinginjava/errorhandle/Ex4/ExceptionSpecification.java
@@ -0,0 +1,14 @@
+package com.cs.hit.thinkinginjava.errorhandle.Ex4;
+
+public class ExceptionSpecification {
+    public static void main(String [] args) {
+        NoExcprion noExcprion=new NoExcprion();
+        try {
+            noExcprion.f();
+            //have to say this
+            //since f() have been specified throw exception
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ hitcs-thinkinginjava/src/main/java/com/cs/hit/thinkinginjava/errorhandle/Ex4/NoExcprion.java
@@ -0,0 +1,7 @@
+package com.cs.hit.thinkinginjava.errorhandle.Ex4;
+
+public class NoExcprion {
+    public void f() throws Exception{
+        System.out.println("f()");
+    }
+}
