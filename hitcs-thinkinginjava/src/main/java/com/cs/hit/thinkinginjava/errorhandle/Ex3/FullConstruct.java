5f258e3e0b5ccc559e0d257cf450609fb37434b9
acceml <1023598954@qq.com>
2016-01-11 22:37:18 +0800

    Rethrow an exception P325

--- hitcs-thinkinginjava/src/main/java/com/cs/hit/thinkinginjava/errorhandle/Ex3/FullConstruct.java
+++ hitcs-thinkinginjava/src/main/java/com/cs/hit/thinkinginjava/errorhandle/Ex3/FullConstruct.java
@@ -15,19 +15,19 @@ public class FullConstruct {
     public static void main(String[] args) {
         try {
             f();
-        }catch (MyException e) {
+        } catch (MyException e) {
             e.printStackTrace(System.out);
         }
         try {
             f();
-        }catch (MyException e) {
+        } catch (MyException e) {
             e.printStackTrace();
         }
 //        is any diff ?
 //        System.out print into console
         try {
             g("hello");
-        }catch (MyException e) {
+        } catch (MyException e) {
             e.printStackTrace();
         }
     }
--- hitcs-thinkinginjava/src/main/java/com/cs/hit/thinkinginjava/errorhandle/Ex4/ExceptionSpecification.java
+++ hitcs-thinkinginjava/src/main/java/com/cs/hit/thinkinginjava/errorhandle/Ex4/ExceptionSpecification.java
@@ -1,13 +1,13 @@
 package com.cs.hit.thinkinginjava.errorhandle.Ex4;
 
 public class ExceptionSpecification {
-    public static void main(String [] args) {
-        NoExcprion noExcprion=new NoExcprion();
+    public static void main(String[] args) {
+        NoExcprion noExcprion = new NoExcprion();
         try {
             noExcprion.f();
             //have to say this
             //since f() have been specified throw exception
-        }catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
