99f74e8c7da6a3eac46d2093a154be30c7def50e
flysalmon <1023598954@qq.com>
2016-01-12 20:48:04 +0800

    exception chain P329

---
+++ hitcs-thinkinginjava/src/main/java/com/cs/hit/thinkinginjava/exception/example/ex3/FullConstruct.java
@@ -0,0 +1,34 @@
+package com.cs.hit.thinkinginjava.exception.example.ex3;
+
+public class FullConstruct {
+    public static void f() throws MyException {
+        System.out.println("Exception from f()");
+        throw new MyException();
+    }
+
+
+    public static void g(String msg) throws MyException {
+        System.out.println("Exception from g()");
+        throw new MyException(msg);
+    }
+
+    public static void main(String[] args) {
+        try {
+            f();
+        } catch (MyException e) {
+            e.printStackTrace(System.out);
+        }
+        try {
+            f();
+        } catch (MyException e) {
+            e.printStackTrace();
+        }
+//        is any diff ?
+//        System.out print into console
+        try {
+            g("hello");
+        } catch (MyException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ hitcs-thinkinginjava/src/main/java/com/cs/hit/thinkinginjava/exception/example/ex4/NoExcprion.java
@@ -0,0 +1,7 @@
+package com.cs.hit.thinkinginjava.exception.example.ex4;
+
+public class NoExcprion {
+    public void f() throws Exception {
+        System.out.println("f()");
+    }
+}
