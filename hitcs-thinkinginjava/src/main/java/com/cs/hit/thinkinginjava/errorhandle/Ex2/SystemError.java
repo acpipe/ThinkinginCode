e31e67cefd5c075d5851f479ec1573c87a648556
flysalmon <1023598954@qq.com>
2016-01-12 20:46:23 +0800

    exception chain P329

--- hitcs-thinkinginjava/src/main/java/com/cs/hit/thinkinginjava/errorhandle/Ex2/SystemError.java
+++
@@ -1,9 +0,0 @@
-package com.cs.hit.thinkinginjava.errorhandle.Ex2;
-
-//P318
-public class SystemError {
-    public static void main(String[] args) {
-        System.err.println("hello");
-        System.out.println("hello");
-    }
-}
--- hitcs-thinkinginjava/src/main/java/com/cs/hit/thinkinginjava/errorhandle/Ex4/NoExcprion.java
+++
@@ -1,7 +0,0 @@
-package com.cs.hit.thinkinginjava.errorhandle.Ex4;
-
-public class NoExcprion {
-    public void f() throws Exception {
-        System.out.println("f()");
-    }
-}
