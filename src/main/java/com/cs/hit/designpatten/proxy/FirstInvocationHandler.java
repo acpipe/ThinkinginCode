a0923087fd52a66e97478acadea6aad27297f877
flysalmon <1023598954@qq.com>
2016-03-10 16:51:39 +0800

    dynamic proxy in reflect

---
+++ src/main/java/com/cs/hit/designpatten/proxy/FirstInvocationHandler.java
@@ -0,0 +1,19 @@
+package com.cs.hit.designpatten.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+public class FirstInvocationHandler implements InvocationHandler{
+    private Object o;
+
+    public FirstInvocationHandler(Object o) {
+        this.o = o;
+    }
+
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("before invoke");
+        method.invoke(o,args);
+        System.out.println("after invoke");
+        return null;
+    }
+}
---
+++ src/main/java/com/cs/hit/designpatten/proxy/TestJavaProxy.java
@@ -0,0 +1,15 @@
+package com.cs.hit.designpatten.proxy;
+
+import java.lang.reflect.Proxy;
+import java.util.concurrent.Callable;
+
+public class TestJavaProxy {
+    public static void main(String[] args) throws Exception {
+        Phone phone = new Phone();
+        Callable proxyPhone = (Callable) Proxy.newProxyInstance(
+                phone.getClass().getClassLoader(),
+                phone.getClass().getInterfaces(),
+                new FirstInvocationHandler(phone));
+        proxyPhone.call();
+    }
+}
